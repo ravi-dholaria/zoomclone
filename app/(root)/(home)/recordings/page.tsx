d10b5ecfcc3dde75cfdd430bae0991a67d99e738
ravi-dholariya <200540107103@darshan.ac.in>
2024-04-04 18:28:42 +0530

    intial commit

---
+++ app/(root)/(home)/recordings/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const recordings = () => {
+  return (
+    <section className="flex size-full flex-col gap-10 text-white">
+      <h1 className="text-3xl font-bold">recordings</h1>
+    </section>
+  );
+};
+
+export default recordings;
---
+++ constants/index.ts
@@ -0,0 +1,27 @@
+export const sidebarLinks = [
+  {
+    label: "Home",
+    route: "/",
+    imgUrls: "icons/Home.svg",
+  },
+  {
+    label: "Upcoming",
+    route: "/upcoming",
+    imgUrls: "icons/upcoming.svg",
+  },
+  {
+    label: "Previous",
+    route: "/previous",
+    imgUrls: "icons/previous.svg",
+  },
+  {
+    label: "Recordings",
+    route: "/recordings",
+    imgUrls: "icons/Video.svg",
+  },
+  {
+    label: "PersonalRoom",
+    route: "/personal-room",
+    imgUrls: "icons/add-personal.svg",
+  },
+];
