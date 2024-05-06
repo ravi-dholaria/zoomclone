42261773cabc2f5bebaa0cf79e5bb2ae1244171b
ravi-dholariya <200540107103@darshan.ac.in>
2024-05-06 19:58:04 +0530

    Metadata Changed

--- app/layout.tsx
+++ app/layout.tsx
@@ -40,7 +40,27 @@ export default function RootLayout({
       <html lang="en">
         <head>
           <title>Yoom</title>
-          <link rel="icon" href="/icons/logo.svg" />
+          <meta
+            name="description"
+            content="A web-based video conferencing application for seamless communication"
+          />
+          <meta
+            name="keywords"
+            content="video conferencing, online meetings, web meetings"
+          />
+          <meta name="author" content="Your Name or Your Company" />
+          <meta
+            property="og:title"
+            content="Yoom - Your Video Conferencing Solution"
+          />
+          <meta
+            property="og:description"
+            content="A web-based video conferencing application..."
+          />
+          <meta
+            property="og:image"
+            content="https://private-user-images.githubusercontent.com/67959015/317983098-f09a8421-67d3-45ce-b9bc-a791cdc2774b.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MTUwMDU2ODksIm5iZiI6MTcxNTAwNTM4OSwicGF0aCI6Ii82Nzk1OTAxNS8zMTc5ODMwOTgtZjA5YTg0MjEtNjdkMy00NWNlLWI5YmMtYTc5MWNkYzI3NzRiLnBuZz9YLUFtei1BbGdvcml0aG09QVdTNC1ITUFDLVNIQTI1NiZYLUFtei1DcmVkZW50aWFsPUFLSUFWQ09EWUxTQTUzUFFLNFpBJTJGMjAyNDA1MDYlMkZ1cy1lYXN0LTElMkZzMyUyRmF3czRfcmVxdWVzdCZYLUFtei1EYXRlPTIwMjQwNTA2VDE0MjMwOVomWC1BbXotRXhwaXJlcz0zMDAmWC1BbXotU2lnbmF0dXJlPWIzOTlmMjk0YjBlODQ5YjllZDdmZmEzY2M0ZTk3OWNmNDYxYjY2NjYwZmIwZGQyY2Q3MjZlY2JlODc2YmU2OGImWC1BbXotU2lnbmVkSGVhZGVycz1ob3N0JmFjdG9yX2lkPTAma2V5X2lkPTAmcmVwb19pZD0wIn0.IaAMgqtP5Rd88zLhTY6UHLF5VPRj0AwAjhuu2EJBuG4"
+          />
         </head>
         <body className={`${inter.className} bg-dark-2`}>
           {children}
